6f91da4728bff9ab3ad0a84084b62738ebddf168
Mark Vink <info@nomore404.nl>
2023-03-23 15:05:05 +0100

    Rebuild file copying because of bug in laravel-mix-copy-watched

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,40 @@
 // webpack.mix.js
-let fs = require("fs");
-let mix = require("laravel-mix");
-require("laravel-mix-copy-watched");
+const fs = require("fs-extra");
+const mix = require("laravel-mix");
+const WatchExternalFilesPlugin = require("webpack-watch-external-files-plugin");
 
 mix.setPublicPath("./dist");
 mix.options({ manifest: false });
 
+//Clean up the dist folder
 let removeFolders = ["/dist/"];
 
 removeFolders.forEach((folder) => {
   fs.rmSync(process.cwd() + folder, { recursive: true, force: true });
 });
 
-mix.copyWatched("src/static/**/*", "dist", { base: "src/static/" });
+//---
 
+//Add more entries as needed
 mix.sass("./src/scss/style.scss", "dist/css");
 mix.js("./src/js/script.js", "dist/js");
 
-mix.browserSync({ server: { baseDir: "dist" } });
+//--
+
+//Trigger compile on every file change so they get copied from ./src to ./dist
+mix.webpackConfig({
+  plugins: [new WatchExternalFilesPlugin({ files: ["./src/**/*"] })],
+});
+
+mix.after((stats) => {
+  //Bug in laravel-mix-copy-watched made me change to this solution
+  fs.copy("./src/static", "./dist", () => {});
+});
+
+//Browsersync triggers on every file change in ./src
+mix.browserSync({
+  server: {
+    baseDir: "dist",
+  },
+  files: ["./src/**/*"],
+});
